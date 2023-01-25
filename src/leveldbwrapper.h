459c20130d60b6193b637adcdf90a557e4282d43
gdiscord <43574012+gdiscord@users.noreply.github.com>
2023-01-25 22:07:35 +0000

    CDBIterator

--- src/leveldbwrapper.h
+++ src/leveldbwrapper.h
@@ -16,6 +16,9 @@
 #include <leveldb/db.h>
 #include <leveldb/write_batch.h>
 
+static const size_t DBWRAPPER_PREALLOC_KEY_SIZE = 64;
+static const size_t DBWRAPPER_PREALLOC_VALUE_SIZE = 1024;
+
 class leveldb_error : public std::runtime_error
 {
 public:

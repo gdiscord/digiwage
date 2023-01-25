c91bb111bead55d4261fc3fd4265f2fc970d713e
gdiscord <43574012+gdiscord@users.noreply.github.com>
2023-01-25 20:09:07 +0000

    Update misc.cpp

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -5,7 +5,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "base58.h"
+//#include "base58.h"
+#include "key_io.h"
 #include "clientversion.h"
 #include "init.h"
 #include "main.h"

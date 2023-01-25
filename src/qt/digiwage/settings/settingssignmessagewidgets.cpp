3d3228f8241d2f8bf774c18bee4aad5c6e0c1502
gdiscord <43574012+gdiscord@users.noreply.github.com>
2023-01-25 20:03:36 +0000

    Update settingssignmessagewidgets.cpp

--- src/qt/digiwage/settings/settingssignmessagewidgets.cpp
+++ src/qt/digiwage/settings/settingssignmessagewidgets.cpp
@@ -8,7 +8,8 @@
 #include "guiutil.h"
 #include "walletmodel.h"
 
-#include "base58.h"
+//#include "base58.h"
+#include "key_io.h"
 #include "init.h"
 #include "wallet/wallet.h"
 #include "askpassphrasedialog.h"
